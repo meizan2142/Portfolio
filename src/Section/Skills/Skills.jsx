d99553f4d0c32dcc4f3b09407c06fb20e718ee72
Your Name01~git <saifsahai2002@gmail.com>
2024-12-08 11:52:09 +0600

    Skills section is almost done

--- src/Section/Skills/Skills.jsx
+++ src/Section/Skills/Skills.jsx
@@ -1,7 +1,8 @@
 import RoundaboutRightRoundedIcon from '@mui/icons-material/RoundaboutRightRounded';
+import { FaArrowRightLong, FaHtml5 } from 'react-icons/fa6';
 const Skills = () => {
     return (
-        <div id="skills">
+        <div id="skills" className="px-9">
             <h1 className="text-[#DCE3E1] text-2xl font-medium flex items-center gap-3">
                 <span className='text-[#3F7D6E]'><RoundaboutRightRoundedIcon /></span>
                 Skills
@@ -9,6 +10,177 @@ const Skills = () => {
                 <span className='text-[#3F7D6E] text-sm'>/</span>
                 <span className="rounded-full border border-[#365E53] py-1 px-2 text-sm text-[#3F7D6E]">tech stack</span>
             </h1>
+            <div className="border border-[#365E53] rounded-md desktop:w-[450px] laptopL:w-[450px] lg:mx-56 desktop:mx-0 md:mx-32 laptopL:mx-0 lg:w-[450px] mt-10 desktop:h-16 mobile:h-32 h-20  p-3 space-y-3">
+                <h1 className="flex items-center gap-2 text-sm font-normal">
+                    <span className='text-[#365E53]'>&gt;_</span>
+                    <span className='text-[#365E53]'>skill</span>
+                    <span className='text-[#365E53]'>/</span>
+                    <span className='text-[#3F7D6E]'>list</span>
+                    <span className='text-[#365E53]'><FaArrowRightLong size={16} /></span>
+                    <span className='text-[#f3f6f5b3]'>Some few technologies and tools I work with to bring ideas to life</span>
+                </h1>
+            </div>
+            <div className='mt-10 desktop:flex gap-6 md:grid laptopL:flex lg:flex '>
+                <div className="desktop:w-[350px] rounded-2xl bg-white shadow-lg dark:bg-[#18181B] md:p-8">
+                    <p>Front-End Tools</p>
+                    <div className=' px-3 grid grid-cols-2'>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="desktop:w-[350px] rounded-2xl bg-white shadow-lg dark:bg-[#18181B] md:p-8">
+                    <p>Front-End Tools</p>
+                    <div className=' px-3 grid grid-cols-2'>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="desktop:w-[350px] rounded-2xl bg-white shadow-lg dark:bg-[#18181B] md:p-8">
+                    <p>Front-End Tools</p>
+                    <div className=' px-3 grid grid-cols-2'>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                        <div className="border border-[#365E53] bg-[#0E1715] rounded-md w-28 mt-6 h-18  p-3 space-y-3">
+                            <div className="flex flex-col items-center gap-3">
+                                <FaHtml5 size={20} color='#3F7D6E' />
+                                <h1 className="text-[#f3f6f5b3] hover:text-[#e7eeedda] font-medium text-xs">HTML</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
